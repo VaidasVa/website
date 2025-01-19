9d9ed06eadfd2031187b348b97e27a036fbefea3
Vaidas <valikonis.vaidas@gmail.com>
2025-01-19 17:43:27 +0200

    Adding router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import Startpage from './pages/Startpage.jsx'
+import StartPage from './pages/StartPage.jsx'
 import Notification from "./components/Notification.jsx";
+import {Route, Routes} from "react-router-dom";
+import Photos from "./pages/Photos.jsx";
+import Dev from "./pages/Dev.jsx";
+import {StrictMode} from "react";
 
 function App() {
 
     return (
         <>
-            <Startpage/>
+            <Routes>
+                <Route path="/" element={<StartPage/>}></Route>
+                <Route path="/photos" element={<Photos/>}></Route>
+                <Route path="/dev" element={<Dev/>}></Route>
+                <Route path="*" element={<StartPage/>} />
+            </Routes>
             <Notification/>
         </>
     )
--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,14 +1,14 @@
 import "../static/style/Startpage.css"
 
-export default function startpage() {
+export default function startPage() {
     return (
         <div className={"startPageContainer"}>
-            <a href={"#"}>
+            <a href={"/dev"}>
                 <div className={"dev"}>
                     <img src="../src/static/images/devLogo.png" alt={"Software Development"}/>
                 </div>
             </a>
-            <a href={"#"}>
+            <a href={"/photos"}>
                 <div className={"photos"}>
                     <img src="../src/static/images/photoLogo.png" alt={"Photography"}/>
                 </div>
