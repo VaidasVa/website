23f8875b715536eb1563189a4e1bf1526266ed34
vaidas <valikonis.vaidas@gmail.com>
2025-01-21 16:19:17 +0200

    Update to photos

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -1,7 +1,7 @@
 import "../static/style/Photos.css"
 import PhotosHeader from "../components/PhotosHeader.module.jsx"
 import PhotosBody from "../components/PhotosBody.module.jsx";
-import PhotosFooter from "../components/PhotosFootage.module.jsx";
+import PhotosFooter from "../components/PhotosFooter.module.jsx";
 
 export default function Photos() {
     return (
