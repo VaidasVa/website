4bbb5851c086cb3bed379b21250f2f5bbe7549cb
vaidas <valikonis.vaidas@gmail.com>
2025-01-28 10:07:10 +0200

    Adding temp solution for GitHub contributions image

--- src/components/Git.module.jsx
+++ src/components/Git.module.jsx
@@ -1,10 +1,12 @@
-export default function Git(){
+export default function Git() {
     return (
         <div className={"git"}>
             <h2>GitHub</h2>
-            <img src="https://ghchart.rshah.org/17172C/vaidasva" alt="Name Your Github chart"
-            style={{width: '100%', maxHeight: '150px'}}
-            />
+            <a href={"https://github.com/VaidasVa/"} target="_blank" rel="noopener noreferrer">
+                <img src="https://ghchart.rshah.org/17172C/vaidasva" alt="Name Your Github chart"
+                     style={{width: '100%', maxHeight: '150px'}}
+                />
+            </a>
         </div>
     )
 }
