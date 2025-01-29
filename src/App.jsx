d2c0c216ec976862c57db508cf365ec77147b560
Vaidas <valikonis.vaidas@gmail.com>
2025-01-29 18:20:53 +0200

    Hiding notification

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import StartPage from './pages/StartPage.jsx'
 import {Route, Routes} from "react-router-dom";
 import Photos from "./pages/Photos.jsx";
 import Dev from "./pages/Dev.jsx";
-import Notification from "./components/Notification.jsx";
 import React, {StrictMode} from "react";
 import Test from "./pages/Test.jsx";
 
@@ -18,7 +17,7 @@ function App() {
                 <Route path="/test" element={<Test/>} />
                 <Route path="*" element={<StartPage/>} />
             </Routes>
-            <Notification />
+            {/*<Notification />*/}
         </StrictMode>
     )
 }
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,18 +1,18 @@
-import {Alert, Snackbar} from "@mui/material";
-import React from "react";
-
-export default function Notification() {
-
-    const [open, setOpen] = React.useState(true);
-    setTimeout(() => setOpen(false), 3000);
-
-    return (
-        <Snackbar open={open} autoHideDuration={3000}>
-            <Alert
-                severity="warning"   variant="filled"
-                sx={{ width: '100%', background:"rgba(255,226,40,0.85)", color:"black" }} >
-                This website is in active development.
-            </Alert>
-        </Snackbar>
-    )
-}
+// import {Alert, Snackbar} from "@mui/material";
+// import React from "react";
+//
+// export default function Notification() {
+//
+//     const [open, setOpen] = React.useState(true);
+//     setTimeout(() => setOpen(false), 3000);
+//
+//     return (
+//         <Snackbar open={open} autoHideDuration={3000}>
+//             <Alert
+//                 severity="warning"   variant="filled"
+//                 sx={{ width: '100%', background:"rgba(255,226,40,0.85)", color:"black" }} >
+//                 This website is in active development.
+//             </Alert>
+//         </Snackbar>
+//     )
+// }
