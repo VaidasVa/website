a146bd6492e822aec2bcd93feeeb8d831887617b
Vaidas <valikonis.vaidas@gmail.com>
2025-01-29 11:25:01 +0200

    Adding tech module for dynamic and reusable icons

--- src/components/Projects.module.jsx
+++ src/components/Projects.module.jsx
@@ -1,52 +1,96 @@
 import projectData from "/src/static/projects.json"
 import icons from "/src/helpers/icons.jsx"
-import {Box, Modal} from "@mui/material";
-import React from "react";
+import {Box, Modal, Tooltip} from "@mui/material";
+import React, {useState} from "react";
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import {FaGithub} from "react-icons/fa";
 
 export default function Projects() {
-    const [modalOpen, setModalOpen] = React.useState(false)
+    const [modalOpen, setModalOpen] = useState(false)
     const [selected, setSelected] = React.useState(0)
 
-    return (<div className={"projects"}>
-        <h2>Projects</h2>
-        <h3>Personal Projects</h3>
-        <div className="personalProjects">
-            {projectData.map((project, index) => (<div className="projectCard" key={index}>
-                <div className={"projectTitle"}>{project.title}</div>
-                <div className={"projectTech"}>
-                    {project.technologies.map((technology) => (icons.find((tech) => technology === tech.id)?.icon))}
-                </div>
-                <button title={"More..."}
-                        onClick={() => {
-                            setModalOpen(true);
-                            setSelected(index)
-                        }}
-                >...
-                </button>
-                <Modal open={modalOpen} onClose={() => {
-                    setModalOpen(false)
-                }}>
-                    <Box direction={"row"} align={"center"}
-                         style={{
-                             backgroundColor: "white",
-                             width: "90dvw",
-                             minHeight: "50dvh",
-                             maxHeight: "90vh",
-                             borderRadius: "10px",
-                             padding: "10px",
-                             margin: "40px auto"
-                         }}>
-                        <div>{projectData[selected].title}</div>
-                        <div>{projectData[selected].id}</div>
-                        <p>hi</p>
-                    </Box>
-                </Modal>
-            </div>))}
-        </div>
-        <h3>Work Projects</h3>
-        <div className="workProjects">
+    function projects(type) {
+        return (<div className="subprojects">
+            {filter(type).map((project, index) => (
+                <div className="projectCard" key={index}>
+                    <div className={"projectTitle"}>
+                        {project.title}
+                    </div>
+                    <div className={"projectTech"}>
+                        {project.technologies.map((technology) => (icons
+                            .filter((tech) => technology === tech.id)
+                            .map((icon) => (
+                                <Tooltip title={icon.id} key={`${icon.id}-${project.title}-${Math.random()}`}>
+                                    <span>{icon.icon}</span>
+                                </Tooltip>))))}
+                    </div>
+                    <Tooltip title={"More info"}>
+                        <button className={"button"}
+                                onClick={() => {
+                                    setModalOpen(true);
+                                    setSelected(project.id - 1)
+                                }}
+                        >
+                            <MoreHorizIcon/>
+                        </button>
+                    </Tooltip>
+                    <Modal open={modalOpen} onClick={() => {
+                        setModalOpen(false)
+                    }}>
+                        <Box direction={"row"} align={"center"} className={"modalBox"}
+                             onAuxClick={() => setModalOpen(false)}>
+                            <div className={"closeButton"} onClick={() => setModalOpen(false)}>
+                                &times;
+                            </div>
+                            <h2>{projectData[selected].title}</h2>
+                            {projectData[selected].category === "personal" ?
+                                (<a href={projectData[selected].github} target={"_blank"} rel={"noreferrer"}>
+                                    <FaGithub fontSize={"30px"} style={{color: "black"}}/>
+                                </a>) : null}
+                            <div className={"projectInfo"}>
+                                <div className={"projectInfo-description"}>{projectData[selected].description}</div>
+                                <div style={{textAlign: "justify"}} className={"projectInfo-tech"}>
+                                    <h5>Technologies used:</h5>
+                                    {projectData[selected].technologies.map((technology) => (icons
+                                        .filter((tech) => technology === tech.id)
+                                        .map((icon) => (
+                                            <ul key={icon.id}>
+                                                <li>
+                                                    <span style={{
+                                                        marginRight: "20px",
+                                                        fontSize: "20px"
+                                                    }}>{icon.icon}</span>
+                                                    <span>{icon.id}</span>
+                                                </li>
+                                            </ul>
+                                        ))))}
+                                </div>
+                            </div>
+                            <div className={"projectInfo-screenshots"}>
 
-        </div>
+                            </div>
+                        </Box>
+                    </Modal>
+                </div>))}
+        </div>)
+    }
 
-    </div>)
+    return (
+        <section title={"My Projects"} className={"projects innerMargin"}>
+            <h2>Projects</h2>
+            <h3 style={{paddingBottom: "-15px"}}>Personal Projects</h3>
+            {projects("personal")}
+            <h3>Work Projects</h3>
+            {projects("work")}
+        </section>
+    )
 }
+
+function filter(type) {
+    let filtered = [];
+    projectData.filter((item) => {
+        item.category === type ? filtered.push(item) : null;
+    })
+    return filtered;
+}
+
--- src/helpers/Icons.jsx
+++ src/helpers/Icons.jsx
@@ -1,6 +1,7 @@
-import {FaAws, FaJava, FaReact} from "react-icons/fa";
-import {SiSpringboot} from "react-icons/si";
+import {FaAws, FaCss3Alt, FaDocker, FaGitAlt, FaHtml5, FaJava, FaJenkins, FaReact} from "react-icons/fa";
+import {SiApachekafka, SiHashicorp, SiMongodb, SiSpring, SiSpringboot, SiThymeleaf} from "react-icons/si";
 import {IoLogoJavascript} from "react-icons/io";
+import {DiMysql, DiRedis} from "react-icons/di";
 
 const icons = [
     {"id": "Java", "icon": <FaJava/>,},
@@ -8,5 +9,25 @@ const icons = [
     {"id": "React", "icon": <FaReact/>,},
     {"id": "Javascript", "icon": <IoLogoJavascript />},
     {"id": "AWS", "icon": <FaAws />},
+    {"id": "Kafka", "icon": <SiApachekafka />},
+    {"id": "MySQL", "icon": <DiMysql />},
+    {"id": "Redis", "icon": <DiRedis />},
+    {"id": "MongoDB", "icon": <SiMongodb />},
+    {"id": "Git", "icon": <FaGitAlt />},
+    {"id": "Jenkins", "icon": <FaJenkins />},
+    {"id": "Docker", "icon": <FaDocker />},
+    {"id":"Apigee", "icon":  <img src={"../src/static/images/apigee.png"} alt={"Apigee logo"}
+                                  style={{height:'1rem', width:'2.5rem', margin:".75rem 0 .75rem 0"}}/>},
+    {"id":"Optimizely", "icon":  <img src={"../src/static/images/optimizely.png"} alt={"Optimizely logo"}
+                                  style={{height:'2.5rem', width:'2.5rem'}}/>},
+   {"id":"jBPM", "icon":  <img src={"https://www.jbpm.org/headerFooter/jBPMLogo_siteheader.png"} alt={"Optimizely logo"}
+                                  style={{height:'auto', width:'2.5rem', padding: '1rem 0',
+                                      filter:"grayscale(1)"}}/>},
+    {"id": "HTML", "icon":  <FaHtml5 />},
+    {"id": "CSS", "icon": <FaCss3Alt />},
+    {"id": "Thymeleaf", "icon": <SiThymeleaf />},
+    {"id": "Spring Cloud Config", "icon": <SiSpring />},
+    {"id": "HashiCorp Vault", "icon": <SiHashicorp />},
 ]
+
 export default icons;
