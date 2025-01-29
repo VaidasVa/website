06fb5c4b1a8ae38c550510ad3799a29d700e5198
Vaidas <valikonis.vaidas@gmail.com>
2025-01-29 11:23:59 +0200

    Git chart layout update

--- src/components/Git.module.jsx
+++ src/components/Git.module.jsx
@@ -1,12 +1,12 @@
 export default function Git() {
     return (
-        <div className={"git"}>
+        <div className={"git innerMargin"}>
             <h2>GitHub</h2>
-            <a href={"https://github.com/VaidasVa/"} target="_blank" rel="noopener noreferrer">
-                <img src="https://ghchart.rshah.org/17172C/vaidasva" alt="Name Your Github chart"
-                     style={{width: '100%', maxHeight: '150px'}}
-                />
-            </a>
+            <div className={"gitChart"}>
+                <a href={"https://github.com/VaidasVa/"} target="_blank" rel="noopener noreferrer">
+                    <img src="https://ghchart.rshah.org/17172C/vaidasva" alt="Github chart"/>
+                </a>
+            </div>
         </div>
     )
 }
