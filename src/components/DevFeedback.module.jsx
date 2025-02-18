1f1c2a9c32b6eeccd3178cbd806ca5be6be63e6e
Vaidas <valikonis.vaidas@gmail.com>
2025-02-18 16:28:17 +0100

    Adjusting Feedback section

--- src/components/DevFeedback.module.jsx
+++ src/components/DevFeedback.module.jsx
@@ -1,48 +1,50 @@
 import data from "/public/feedback.json"
 import React from "react";
-import { BsArrowsAngleExpand } from "react-icons/bs";
+import {BsArrowsAngleExpand} from "react-icons/bs";
+import ReactMarkdown from "react-markdown";
 
+function setFeedbackCardSize(index) {
+    const selectedCard = document.getElementsByClassName('feedbackCard')[index];
+    const {height: currentHeight, width: currentWidth} = selectedCard.getBoundingClientRect();
+    let contentHeight;
+
+    if (currentHeight < 277) {
+        contentHeight = selectedCard.querySelector('.feedbackContent').scrollHeight;
+        selectedCard.style.maxHeight = `${contentHeight}px`;
+        setWidth(selectedCard, currentWidth);
+    } else {
+        selectedCard.style.maxHeight = "256px";
+        setWidth(selectedCard, currentWidth);
+    }
+}
+
+const setWidth = (selectedCard, currentWidth) => {
+    return currentWidth < 300 ? selectedCard.style.width = "100%" : selectedCard.style.width = "280px";
+}
 
 export default function DevFeedback() {
-
-    // todo: parse feedback json object
-    // todo flex div with cards
-    // todo how to trim text and add ... to indicate there's more?
-
-    // const textContainer = document.querySelector('.feedbackContent');
-    // const moreBtn = document.querySelector('.more-btn');
-    //
-    // if (textContainer.scrollHeight > textContainer.clientHeight) {
-    //     moreBtn.style.display = 'block';
-    // } else {
-    //     moreBtn.style.display = 'none';
-    // }
-
-    // document.querySelector(".more-btn").addEventListener("click", () =>
-    //     document.querySelector('.feedbackCard').style.height="600px")
-
     return (
         <div className={"devFeedback innerMargin"}>
             <h2>What my colleagues say</h2>
             <div className={"allFeedbacks"}>
-            {data.map((item, index) => (
-                <div className="feedbackCard" key={index}>
-                    <div><h5>{item.name}</h5></div>
-                    <div className={"feedbackContent"}>{item.feedback}</div>
-                    <div className={"more-btn"}
-                         style={{
-                             // padding: "10px 0 0 0",
-                             display: item.feedback.length > 100 ? 'block' : 'none'
-                         }}
-                         onClick={() => {
-                             const selectedCard = document.getElementsByClassName('feedbackCard')[index];
-                             const contentHeight = selectedCard.querySelector('.feedbackContent').scrollHeight;
-                             selectedCard.style.maxHeight.substring(0, selectedCard.style.maxHeight.length-2) < 170 ?
-                                  selectedCard.style.maxHeight = contentHeight + 50 + 'px' : selectedCard.style.maxHeight = "156px";
-                    }}>
-                        <BsArrowsAngleExpand/></div>
-                </div>
-            ))}
+                {data.map((item, index) => (
+                    <div className="feedbackCard" key={index}>
+                        <div><h5>{item.name}</h5></div>
+                        <div className={"feedbackContent"}>
+                            <ReactMarkdown>
+                                {item.feedback}
+                            </ReactMarkdown>
+                        </div>
+                        <div className={"more-btn"}
+                             style={{display: item.feedback.length < 240 ? 'none' : 'block'}}
+                             onClick={() => setFeedbackCardSize(index)}
+                             onTouchEnd={(e) => {
+                                 setFeedbackCardSize(index);
+                             }}>
+                            <div><BsArrowsAngleExpand/></div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
