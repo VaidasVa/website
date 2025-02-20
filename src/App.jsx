fbeeb9d2bd5e8dac211f69103bad55511ff4cdc4
Vaidas <valikonis.vaidas@gmail.com>
2025-02-20 17:58:54 +0100

    Adding curves to main page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,10 @@ import StartPage from './pages/StartPage.jsx'
 import {Route, Routes} from "react-router-dom";
 import Photos from "./pages/Photos.jsx";
 import Dev from "./pages/Dev.jsx";
-import React, {StrictMode} from "react";
 import Test from "./pages/Test.jsx";
 
 function App() {
-
-
     return (
-        // todo remove strict mode for prod
-        // <StrictMode>
             <Routes>
                 <Route path="/" element={<StartPage/>} />
                 <Route path="/photos" element={<Photos/>} />
@@ -18,7 +13,6 @@ function App() {
                 <Route path="/test" element={<Test/>} />
                 <Route path="*" element={<StartPage/>} />
             </Routes>
-
     )
 }
 
--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -3,10 +3,13 @@ import "/public/style/Startpage.css"
 export default function startPage() {
     return (
         <div className={"startPageContainer"}>
+            <div className="box-inner"></div>
             <a href={"/dev"}>
                 <div className={"dev"}>
                     <img src="/images/devLogo.png" alt={"Software Development"}/>
+
                 </div>
+
             </a>
             <a href={"/photos"}>
                 <div className={"photos"}>
